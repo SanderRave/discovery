c7af9eeaf025dd7fa0fdc98a97c5d52a0844ba76
SanderRave <sanderrave@outlook.com>
2025-02-18 22:56:40 +0100

    Newsletter button honing

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -4,7 +4,7 @@ const Newsletter = () => {
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4">
         {/* Text Section */}
         <div className="lg:col-span-2 my-4">
-          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-4">
             Want tips & tricks to optimize your flow?
           </h1>
           <p>Sign up to our newsletter and stay up to date.</p>
@@ -18,7 +18,7 @@ const Newsletter = () => {
               type="email"
               placeholder="Enter Email"
             />
-            <button className="bg-[#00df9a] w-[200px] sm:w-auto rounded-md font-medium px-6 py-3 text-black mt-4 sm:mt-0 sm:ml-4">
+            <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black sm:ml-4">
               Notify me
             </button>
           </form>
